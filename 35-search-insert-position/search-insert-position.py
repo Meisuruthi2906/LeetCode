180e4c2a1f0bd216d80d4a569b18e5042d36c676
Meisuruthi2906 <meisuruthir2906@gmail.com>
2025-03-06 15:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (89.73%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        for index, value in enumerate(nums):
+            if value >= target:
+                return index
+        return len(nums)
