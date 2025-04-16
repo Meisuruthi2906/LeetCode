8b840bfd48dee7144ea47ec1875f40916d2543cb
Meisuruthi2906 <meisuruthir2906@gmail.com>
2025-04-16 16:16:46 +0530

    Time: 5 ms (83.64%) | Memory: 12.5 MB (25.00%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        unique_emails = set()
+        for email in emails:
+            local,domain=email.split('@')
+            local=local.split('+')[0]
+            local=local.replace('.','')
+            normalized_email = local + '+' + domain
+            unique_emails.add(normalized_email)
+        return len(unique_emails)        
