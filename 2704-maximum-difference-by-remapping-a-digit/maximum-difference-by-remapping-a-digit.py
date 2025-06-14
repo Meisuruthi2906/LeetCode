1c97d9f42a6b139c9c35c5f1a1fc42aec34b7f41
Meisuruthi2906 <meisuruthir2906@gmail.com>
2025-06-14 06:31:25 +0530

    Time: 4 ms (28.57%) | Memory: 12.5 MB (14.29%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.py
@@ -0,0 +1,14 @@
+class Solution:
+  def minMaxDifference(self, num):
+    s = str(num)
+    to9 = s[self._firstNotNineIndex(s)]
+    to0 = s[0]
+    return int(s.replace(to9, '9')) - int(s.replace(to0, '0'))
+
+  def _firstNotNineIndex(self, s):
+    for i, c in enumerate(s):
+      if c != '9':
+        return i
+    return 0
+
+
