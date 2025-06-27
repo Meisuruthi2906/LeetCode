e17bebe7ab914c50c780b11b7550dae14274eac2
Meisuruthi2906 <meisuruthir2906@gmail.com>
2025-06-27 20:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (64.14%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numDecodings(self, s):
+        f, g = 0, 1
+        for i, c in enumerate(s, 1):
+            h = g if c != "0" else 0
+            if i > 1 and s[i - 2] != "0" and int(s[i - 2 : i]) <= 26:
+                h += f
+            f, g = g, h
+        return g
+        
