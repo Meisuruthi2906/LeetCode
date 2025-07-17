f90d8f34c5f3192221677cb31dfdcd150a36e34d
Meisuruthi2906 <meisuruthir2906@gmail.com>
2025-07-17 20:52:45 +0530

    Time: 3 ms (34.54%) | Memory: 12.5 MB (98.32%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,9 @@
+class Solution:
+  def hIndex(self, citations):
+    n = len(citations)
+    citations.sort()
+    for i, citation in enumerate(citations):
+      if citation >= n - i:
+        return n - i
+
+    return 0
