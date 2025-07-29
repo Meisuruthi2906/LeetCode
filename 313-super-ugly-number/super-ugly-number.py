8adea8d8d1b9156f599fe651b214f84ede7c15ff
Meisuruthi2906 <meisuruthir2906@gmail.com>
2025-07-29 21:08:45 +0530

    Time: 1533 ms (34.64%) | Memory: 15.6 MB (95.42%) - LeetSync

---
+++ 313-super-ugly-number/super-ugly-number.py
@@ -0,0 +1,16 @@
+class Solution:
+  def nthSuperUglyNumber(self, n, primes):
+    k = len(primes)
+    nums = [1]
+    indices = [0] * k
+    while len(nums) < n:
+      nexts = [0] * k
+      for i in range(k):
+        nexts[i] = nums[indices[i]] * primes[i]
+      next = min(nexts)
+      for i in range(k):
+        if next == nexts[i]:
+          indices[i] += 1
+      nums.append(next)
+
+    return nums[-1]
