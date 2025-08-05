54d72319b373f475be8af925c7428516496efd93
Meisuruthi2906 <meisuruthir2906@gmail.com>
2025-08-05 21:38:25 +0530

    Time: 32 ms (67.54%) | Memory: 12.6 MB (0.88%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -1,11 +1,5 @@
 class Solution(object):
     def numOfUnplacedFruits(self, fruits, baskets):
-        """
-        :type fruits: List[int]
-        :type baskets: List[int]
-        :rtype: int
-        """
-    
         c=0
         for i in range(len(fruits)):
             for j in range(len(baskets)):
