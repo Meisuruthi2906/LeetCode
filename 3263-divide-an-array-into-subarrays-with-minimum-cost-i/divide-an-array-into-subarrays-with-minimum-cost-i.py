8dd1267ce849ba8c6404db8a3bdd47790edf4740
Meisuruthi2906 <meisuruthir2906@gmail.com>
2026-02-01 09:31:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (24.24%) - LeetSync

---
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minimumCost(self, nums):
+        ans = nums[0]
+        first_min = 51
+        second_min = 51
+
+        for num in nums[1:]:
+            if num < first_min:
+                second_min = first_min
+                first_min = num
+            elif num < second_min:
+                second_min = num
+
+        return ans + first_min + second_min
+        
