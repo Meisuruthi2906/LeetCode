943108924ee87916aa5f5044daa7d33355dcb983
Meisuruthi R <meisuruthir2906@gmail.com>
2026-04-16 22:22:33 +0530

    Time: 119 ms (46.46%) | Memory: 190.3 MB (28.28%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -0,0 +1,38 @@
+class Solution {
+    
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        ArrayList<Integer> ans = new ArrayList<>();
+    	
+    	HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+    	
+    	for(int i =0; i < nums.length; i++) {
+    		map.putIfAbsent(nums[i], new ArrayList<>());
+    		map.get(nums[i]).add(i);
+    	}
+    	
+    	for(int i = 0; i < queries.length; i++) {
+    		int target = nums[queries[i]];
+    		ArrayList<Integer> arr =  map.get(target);
+    		if(arr.size() == 1) ans.add(-1);
+    		else {    			
+    			ans.add(minDistance(queries[i], arr, nums.length));
+    		}
+    		
+    	}
+    	
+    	return ans;
+    }
+
+    public static int minDistance(int q , ArrayList<Integer> arr, int n) {
+    	int minDist = Integer.MAX_VALUE;
+        int pos = Collections.binarySearch(arr, q);
+
+    	int left = arr.get((pos - 1 + arr.size()) % arr.size());
+        minDist = Math.min(minDist, Math.min(Math.abs(q-left), (n - Math.abs(q - left))));
+
+        int right = arr.get((pos + 1) % arr.size());
+        minDist = Math.min(minDist, Math.min(Math.abs(q-right), (n - Math.abs(q - right))));
+
+    	return minDist;
+    }
+}
