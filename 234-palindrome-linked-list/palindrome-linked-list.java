620d712b5d9db3b7449004e0f71761552d7e6175
Meisuruthi R <meisuruthir2906@gmail.com>
2026-06-16 15:52:37 +0530

    Time: 3 ms (99.79%) | Memory: 94.4 MB (66.18%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,49 +9,32 @@
  * }
  */
 class Solution {
-
-    public ListNode findMid(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return slow;
-    }
-
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast=fast.next.next;
         }
-
-        ListNode midNode = findMid(head);
-
-        ListNode prev = null;
-        ListNode curr = midNode;
-        ListNode next;
-
-        while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        if(fast!=null){
+            slow=slow.next;
         }
-
-        ListNode right = prev;
-        ListNode left = head;
-
-        while (right != null) {
-            if (right.val != left.val) {
+        ListNode prev=null;
+        while(slow!=null){
+            ListNode next=slow.next;
+            slow.next=prev;
+            prev=slow;
+            slow=next;
+        }
+        ListNode first=head;
+        ListNode second=prev;
+        while(second!=null){
+            if(first.val!=second.val){
                 return false;
             }
-
-            left = left.next;
-            right = right.next;
+            first=first.next;
+            second=second.next;
         }
-
         return true;
     }
 }
